8cf262ced2042b8ea7b9bf2f3cc3a6e9013e0a7c
tina-pina <christina.koenig.de@gmail.com>
2019-02-15 13:49:09 +0100

    created functionality to show random quotes

--- js/script.js
+++ js/script.js
@@ -7,49 +7,69 @@ FSJS project 1 - A Random Quote Generator
 
 
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
+  array of quote objects 
 ***/
 
+let quotes = [{
+    quote: "lovers like to tease each other",
+    source: "S. Mimura",
+    year: 2017,
+    citation: "PONS.com"
+  },
+  {
+    quote: "A TRUE boyfriend is someone that will take care of his girlfriend, stand up for her when needed and love her for who she is and not pretend to care about her!",
+    source: "Nishan Panwar"
+  },
+  {
+    quote: "We come to love not by finding a perfect person, but by learning to see an imperfect person perfectly.",
+    source: "Sam Keen"
+  },
+  {
+    quote: "If you fight like a married couple, talk like bestfriends, flirt like first lovers, protect each other like siblings, you're meant to be.",
+    source: "Tyga"
+  },
+  {
+    quote: "Someone who really loves you sees what a mess you can be, how moody you can get, how hard you are to handle, but still wants you.",
+    source: "unknown"
+  },
 
+]
+
+/***
+  takes the quotes from above and generates a random quote
+***/
+
+function getRandomQuote(quotes) {
+  let allQuotesNum = quotes.length;
+
+  let randomNum = Math.floor(Math.random() * (allQuotesNum));
+  return quotes[randomNum];
+}
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
+  each time we call this function it prints a formated quote
 ***/
 
+function printQuote() {
 
+  let randomQuote = getRandomQuote(quotes);
+
+  let quoteHTML = "";
+  quoteHTML += `<p class="quote">${randomQuote.quote}</p>`
+  quoteHTML += '<p class="source">'
+  if (randomQuote.source) quoteHTML += randomQuote.source;
+  if (randomQuote.citation) quoteHTML += `<span class="citation"> ${randomQuote.citation} </span>`
+  if (randomQuote.year) quoteHTML += `<span class="year"> ${randomQuote.year} </span>`
+  quoteHTML += '</p>'
+
+  document.getElementById("quote-box").innerHTML = quoteHTML;
+
+}
 
 
 /***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
+  each time we click the event listener we show another random quote
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
